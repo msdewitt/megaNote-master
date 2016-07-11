d206e55a59523642487a7a78044acf7ea49dc76c
Matheu DeWitt <msdewitt@bsu.edu>
2016-07-11 09:50:45 -0400

    Display users name when logged in

---
+++ app/users/user-links.directive.js
@@ -0,0 +1,29 @@
+{
+  angular.module('meganote.users')
+    .directive('userLinks', [
+
+
+      'CurrentUser',
+      (CurrentUser) => {
+
+        class UserLinksController{
+          user(){
+            return CurrentUser.get();
+          }
+          signedIn(){
+            return CurrentUser.signedIn();
+          }
+        }
+
+        return{
+          scope: {},
+          controller: UserLinksController,
+          controllerAs: 'vm',
+          bindToController: true,
+          template: `
+                      <div class= "user-links" ng-show="vm.signedIn()">
+                      Signed in as  {{ vm.user().name}}
+                      </div>`,
+        };
+      }]);
+}
